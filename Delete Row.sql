bd3d9c51b7fb76667ff89b27ba1f3257660e7361
IC <icbroqueza13@gmail.com>
2022-11-18 17:16:31 +0800

    first commit

---
+++ Delete Row.sql
@@ -0,0 +1,5 @@
+-- Suppose you want to delete a row whose id = 2 from table users, what would you write to delete the said row?
+
+
+stacktrekclass=# DELETE FROM users
+stacktrekclass-# WHERE id = 2;
